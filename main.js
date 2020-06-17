7d80f3dda59f73ec2741b9bc473f069fa375756e
Sherif Elmetwally <sge5@njit.edu>
2020-06-16 21:00:20 -0400

    FEATURE: render bubblez on the screen

--- main.js
+++ main.js
@@ -16,4 +16,47 @@ d3.csv("data/jobs.csv", function(error, data) {
     let filteredOccupations = occupations.filter(obj => obj.employed > 0)
     filteredOccupations.sort((a,b) => a.employed - b.employed)
     console.log('occupations after sort: ', filteredOccupations)
+
+    let height = 1200;
+    let width = 1500;
+
+    var svg = d3.select("#chart")
+        .append("svg")
+        .attr("height", height)
+        .attr("width", width)
+        .append("g")
+        .attr("transform", "translate(0,0)")
+
+    let radiusScale = d3.scaleSqrt().domain([30, 4528570]).range([5, 50])
+    
+    var circles = svg.selectAll(".occupation")
+        .data(filteredOccupations)
+        .enter().append("circle")
+        .attr("class", "artist")
+        .attr("r", function(d) {
+            return radiusScale(d.employed)
+        })
+        .attr("fill", "#eceaea")
+        .attr("stroke", "grey")
+
+    // the simulation is a collection of forces about where we want our circles to go and how we want them to interact
+    const simulation = d3.forceSimulation()
+        .force("x", d3.forceX(width).strength(0.01))
+        .force("y", d3.forceY(height).strength(0.01))
+        .force("collide", d3.forceCollide(function(d) {
+            return radiusScale(d.employed) + 3
+        }))
+
+    simulation.nodes(filteredOccupations)
+        .on('tick', ticked)
+        
+    function ticked() {
+        circles
+            .attr("cx", function(d) {
+                return d.x
+            })
+            .attr("cy", function(d) {
+                return d.y
+            })
+    }
 })
