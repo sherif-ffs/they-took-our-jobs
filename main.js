a6644fff689c783cafd4d761858f4630b580dda2
Sherif Elmetwally <sge5@njit.edu>
2020-06-17 22:25:46 -0400

    fix label

--- main.js
+++ main.js
@@ -19,68 +19,57 @@ d3.csv("data/jobs.csv", function(error, data) {
 
     let height = 1200;
     let width = 1500;
+    let margin = 40;
+    var labelX = 'X';
+    var x = d3.scaleLinear()
+        .domain([d3.min(data, function (d) { 
+            return 0;
+        }), d3.max(data, function (d) { return d.probability; })])
+        .range([0, width]);
 
-    var tooltip = d3.select("#chart")
-    .append("div")
-      .style("opacity", 0)
-      .attr("class", "tooltip")
-      .style("background-color", "black")
-      .style("border-radius", "5px")
-      .style("padding", "10px")
-      .style("color", "white")
-
-  // -2- Create 3 functions to show / update (when mouse move but stay on same circle) / hide the tooltip
-  var showTooltip = function(d) {
-    tooltip
-      .transition()
-      .duration(200)
-    tooltip
-      .style("opacity", 1)
-      .html(
-          "Occupation: " + d.job +
-          "Peopled employed : " + d.employed + 
-          "Probability of automation : " + d.probability
-      )
-      .style("left", (d3.mouse(this)[0]+30) + "px")
-      .style("top", (d3.mouse(this)[1]+30) + "px")
-  }
-
-  var hideTooltip = function(d) {
-    tooltip
-      .transition()
-      .duration(200)
-      .style("opacity", 0)
-  }
-
+    let peopleEmployed = 'Employed People'
+    let likelyhood = 'Likelyhood of automation'
+    const tip = d3.tip()
+    .attr('class', 'first-d3-tip')
+    .offset([-10, 0])
+    .html(function(d) {
+        return `
+        <div class="scatterplot-tooltip">
+            <h3>${d.job}</h3>
+            <h4>${peopleEmployed}: ${d.employed}</h4>
+            <h4>${likelyhood}: ${d.probability * 100}%</h4>
+        </div>
+        `
+    })
     var svg = d3.select("#chart")
         .append("svg")
         .attr("height", height)
         .attr("width", width)
         .append("g")
         .attr("transform", "translate(0,0)")
+    
+    svg.call(tip);
 
     let radiusScale = d3.scaleSqrt().domain([30, 4528570]).range([5, 50])
     
     var circles = svg.selectAll(".occupation")
         .data(filteredOccupations)
         .enter().append("circle")
-        .attr("class", "artist")
+        .attr("class", "bubble")
         .attr("r", function(d) {
             return radiusScale(d.employed)
         })
         .attr("fill", "#eceaea")
-        .attr("stroke", "grey")
-        // -3- Trigger the functions for hover
-        .on("mouseover", showTooltip )
-        // .on("mousemove", moveTooltip )
-        .on("mouseleave", hideTooltip )
-    // the simulation is a collection of forces about where we want our circles to go and how we want them to interact
-    const simulation = d3.forceSimulation()
-        .force("x", d3.forceX(width).strength(0.01))
-        .force("y", d3.forceY(height).strength(0.01))
-        .force("collide", d3.forceCollide(function(d) {
-            return radiusScale(d.employed) + 3
-        }))
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide)
+    // the simulation is a collection of forces about where we want our circles to go and 
+    // how we want them to interact
+        const simulation = d3.forceSimulation()
+            .force("x", d3.forceX(width/2).strength(0.1))
+            .force("y", d3.forceY(height/2).strength(0.1))
+            .force("collide", d3.forceCollide(function(d) {
+                return radiusScale(d.employed) + 3
+            }))
 
     simulation.nodes(filteredOccupations)
         .on('tick', ticked)
